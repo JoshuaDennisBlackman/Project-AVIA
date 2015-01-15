ef6a98cd05249f7f3a551243b00f54f888b2c0e4
Joshua Dennis Blackman <jhawsh@gmail.com>
2015-01-15 21:22:46 +0000

    Base
    Network Base

---
+++ NetworkClient/NetworkClient/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net;
+
+namespace NetworkClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"╔═╗╦═╗╔═╗ ╦╔═╗╔═╗╔╦╗  ╔═╗╦  ╦╦╔═╗  ╔═╗╦  ╦╔═╗╔╗╔╦╗");
+            Console.WriteLine(@"╠═╝╠╦╝║ ║ ║║╣ ║   ║   ╠═╣╚╗╔╝║╠═╣  ║  ║  ║║╣ ║║║║ ");
+            Console.WriteLine(@"╩  ╩╚═╚═╝╚╝╚═╝╚═╝ ╩   ╩ ╩ ╚╝ ╩╩ ╩  ╚═╝╩═╝╩╚═╝╝╚╝╩");
+           
+
+            byte[] message = new byte[4096];
+            int bytesRead;
+
+            TcpClient client = new TcpClient();
+
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 3000);
+
+            client.Connect(serverEndPoint);
+
+            Console.WriteLine("[CLIENT] Connected to server.");
+
+            NetworkStream clientStream = client.GetStream();
+
+            ASCIIEncoding encoder = new ASCIIEncoding();
+
+            while (true)
+            {
+                string input = null;
+                input = Console.ReadLine();
+                if (input != "")
+                {
+                    byte[] buffer = encoder.GetBytes(input);
+                    clientStream.Write(buffer, 0, buffer.Length);
+                }
+                else if (input == "quit")
+                {
+                    break;
+                }
+            }
+            clientStream.Flush();
+        }
+    }
+}
---
+++ NetworkServer/NetworkServer/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace NetworkServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"╔═╗╦═╗╔═╗ ╦╔═╗╔═╗╔╦╗  ╔═╗╦  ╦╦╔═╗  ╔═╗╔═╗╦═╗╦  ╦╔═╗╦═╗");
+            Console.WriteLine(@"╠═╝╠╦╝║ ║ ║║╣ ║   ║   ╠═╣╚╗╔╝║╠═╣  ╚═╗║╣ ╠╦╝╚╗╔╝║╣ ╠╦╝");
+            Console.WriteLine(@"╩  ╩╚═╚═╝╚╝╚═╝╚═╝ ╩   ╩ ╩ ╚╝ ╩╩ ╩  ╚═╝╚═╝╩╚═ ╚╝ ╚═╝╩╚═");
+           
+            Server s = new Server();
+
+        }
+    }
+
+    class Server
+    {
+        private TcpListener tcpListener;
+        private Thread listenThread;
+
+        public Server()
+        {
+            this.tcpListener = new TcpListener(IPAddress.Any, 3000);
+            this.listenThread = new Thread(new ThreadStart(ListenForClients));
+            this.listenThread.Start();
+        }
+
+        private void ListenForClients()
+        {
+            this.tcpListener.Start();
+
+            while (true)
+            {
+                //blocks until a client has connected to the server
+                TcpClient client = this.tcpListener.AcceptTcpClient();
+
+                //create a thread to handle communication 
+                //with connected client
+                Thread clientThread = new Thread(new ParameterizedThreadStart(HandleClientComm));
+                clientThread.Start(client);
+            }
+        }
+
+        private void HandleClientComm(object client)
+        {
+            TcpClient tcpClient = (TcpClient)client;
+            NetworkStream clientStream = tcpClient.GetStream();
+
+            byte[] message = new byte[4096];
+            int bytesRead;
+            Console.WriteLine("[SERVER] " + tcpClient.Client.RemoteEndPoint + " has connected.");
+            while (true)
+            {
+                bytesRead = 0;
+
+                try
+                {
+                    //blocks until a client sends a message
+                    bytesRead = clientStream.Read(message, 0, 4096);
+                    ASCIIEncoding encoder = new ASCIIEncoding();
+                    Console.WriteLine("[CLIENT " + tcpClient.Client.RemoteEndPoint + "] " + encoder.GetString(message, 0, bytesRead));
+                }
+                catch
+                {
+                    //a socket error has occured
+                    break;
+                }
+
+                if (bytesRead == 0)
+                {
+                    //the client has disconnected from the server
+                    break;
+                }
+            }
+
+            Console.WriteLine("[SERVER] " + tcpClient.Client.RemoteEndPoint + " has disconnected from the server.");
+            tcpClient.Close();
+            
+        }
+    }
+}
